012e2c78d7494571b6c124f5b9372b8bbb59cc70
vbrunel <v.brunel@live.fr>
2017-01-28 09:42:41 +0100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name documentsApp
+ * @description
+ * # documentsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('documentsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name documentsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the documentsApp
+ */
+angular.module('documentsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
